e8cb84cbb062f6791e216351cf8b2823f2ca40cf
Mentor Calekyz <caleborenge8@gmail.com>
2026-09-13 00:43:23 +0300

    Update main.tsx

--- web/mt5react/src/main.tsx
+++ web/mt5react/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Service worker temporarily disabled to fix cache issues.
-// Will be re-added after the site is stable.
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
